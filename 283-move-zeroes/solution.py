80bed0999f1e955694eea30baa2e90459f9462bf
Sanjiv Kumar <sksanjiv77@gmail.com>
2020-10-06 12:01:39 +0530

    Add solution

--- 283-move-zeroes/solution.py
+++ 283-move-zeroes/solution.py
@@ -1,10 +1,14 @@
-
 class Solution:
     def moveZeroes(self, nums):
         """
         Do not return anything, modify nums in-place instead.
         """
         l, r = 0,0
+        # for num in nums:
+        #     if num > 0:
+        #         r = nums.index(num)
+        #         break
+        #for i in range(len(nums)-1):
         while(r <= len(nums)-1 and l<= len(nums)-1):
             if nums[l] !=0:
                 l += 1
@@ -15,9 +19,11 @@ class Solution:
             
             if l<r:
                 nums[l], nums[r] = nums[r], nums[l]
+            #print(nums)
 
             if nums[l] != 0:
                 l += 1
-        
+            
+            #if nums[r] == 0:
             r += 1
             
